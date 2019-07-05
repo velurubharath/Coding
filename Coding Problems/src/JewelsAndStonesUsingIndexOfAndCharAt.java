0fd5a899456e86e017c38eaf962bef549feedaf2
velurubharath <velurubharath@gmail.com>
2019-07-05 09:26:51 +0530

    JewelsAndStones With CharAt and IndexOf for O(m+n)

--- Coding Problems/src/JewelsAndStonesUsingIndexOfAndCharAt.java
+++ Coding Problems/src/JewelsAndStonesUsingIndexOfAndCharAt.java
@@ -3,7 +3,28 @@ public class JewelsAndStonesUsingIndexOfAndCharAt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String S = "aAAAbbbb";
+		String J = "aA";
+		
+		
+		JewelsInStones(S,J);
 
+	}
+
+	private static int JewelsInStones(String S, String J) {
+		// TODO Auto-generated method stub
+		
+		int result = 0;
+		
+		for(int i=0;i<S.length();i++) {
+			if(J.indexOf(S.charAt(i))!=-1)
+				result++;
+		}
+		
+		System.out.println("Result is "+result);
+		return result;
+		
 	}
 
 }
