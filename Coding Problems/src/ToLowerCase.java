42225de1bdece5443a5e1a12955e10f23e6396da
velurubharath <velurubharath@gmail.com>
2019-07-05 09:35:17 +0530

    To convert to lower case

--- Coding Problems/src/ToLowerCase.java
+++ Coding Problems/src/ToLowerCase.java
@@ -1,4 +1,23 @@
 
 public class ToLowerCase {
+	
+	public static void main(String args[]) {
+		
+		/*
+		 * Converting the string to lowercase
+		 */
+		
+		String str = "Hello";
+		toLowerCase(str);
+		System.out.println(toLowerCase(str));
+
+	}
+
+	private static String toLowerCase(String str) {
+		// TODO Auto-generated method stub
+		
+		return str.toLowerCase();
+		
+	}
 
 }
