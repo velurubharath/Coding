a31574ae4767f8a03b95e9059e3e606a3d7cf923
velurubharath <velurubharath@gmail.com>
2019-07-08 00:39:54 +0530

    ProbabilitiesOfAString

--- Coding Problems/src/ProbablitiesOfAString.java
+++ Coding Problems/src/ProbablitiesOfAString.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class AddTwoStringsOfBinaryNumbers {
+public class ProbablitiesOfAString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
